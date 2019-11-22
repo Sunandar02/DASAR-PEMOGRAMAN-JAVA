87f96b832e7a0082fa8c265b805c06255f03a1e8
Sunandar02 <famznanz02@gmail.com>
2019-11-22 18:14:17 +0700

    Sunandar

---
+++ Latihan 1 Menampilkan/src/menampilkan/Menampilkan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menampilkan;
+
+/**
+ *
+ *  
+ * Nama : Sunandar 
+ * KELAS : TI-1C
+ * NIM : A2.1900173
+ */
+public class Menampilkan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata Diri Sendiri" + "\n" + "-------------------");
+        System.out.println("NAMA\t\t: Sunandar");
+        System.out.println("ALAMAT\t\t: MALANGBONG");
+        System.out.println("TEMPAT LAHIR\t: GARUT");
+        System.out.println("TANGGAL LAHIR\t: 02 November 2000");
+        System.out.println("-------------------");
+        System.out.println("HOBI\t\t: VOLLY BALL");
+        System.out.println("MAKES\t\t: NASI GORENG");
+        System.out.println("MIKES\t\t: Teh manis");
+    }
+}
---
+++ latihan6 static dan konstanta/src/latihan6/Latihan6.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan6;
+
+/**
+ *
+ * NAMA : Sunandar
+ * KELAS: TI-1C
+ * NIM  : A2.1900173
+ */
+public class Latihan6 {
+        //variabel jumlahKambing dideklarasikan sebagai statik
+        public static int jumlahKambing;
+    }
+    class KambingStatic{
+    //NAMA_KAMBING sebagai konstanta
+    public static final String NAMA_KAMBING = " MIDUN "; 
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Latihan6.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + Latihan6.jumlahKambing);
+    }
+}
